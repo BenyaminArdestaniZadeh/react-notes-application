039a65cc1247681a8e8c725bbddb42fcc9fe4dcf
benyamin ardestani zadeh <benyaminardestanizadeh@outlook.com>
2024-01-06 15:14:00 +0330

    feat: define jotai provider for app

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import App from "./App.tsx";
 import "./index.css";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
+import { Provider } from "jotai";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Theme>
-      <App />
-    </Theme>
+    {/* use jotai provider to handle global states */}
+    <Provider>
+      <Theme>
+        <App />
+      </Theme>
+    </Provider>
   </React.StrictMode>
 );
