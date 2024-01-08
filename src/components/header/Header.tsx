fd6945fb6a37088bc1be566521ee4067c7f070dc
benyamin ardestani zadeh <benyaminardestanizadeh@outlook.com>
2024-01-08 15:20:13 +0330

    feat(search): add search icon and improve styling for search input

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,20 @@
 import { Button, Flex, Text } from "@radix-ui/themes";
 import { ReactSVG } from "react-svg";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 
 const Header = () => {
+  const [searchValue, setSearchValue] = useState<string | number | null>(null);
   return (
     <form>
       <Flex direction={"column"} gap={"4"}>
-        <input className="search-input" placeholder="Search Notes" />
+        <Flex gap={"2"} align={"center"} className="search-wrapper">
+          <MagnifyingGlassIcon
+            style={{ color: "whitesmoke", width: "25px", height: "25px" }}
+          />
+          <input className="search-input" placeholder="Search Notes" />
+        </Flex>
+
         <Flex gap={"5"} align={"center"} py={"3"}>
           <Button size={"3"} color="yellow" style={{ cursor: "pointer" }}>
             <Text>All</Text>
