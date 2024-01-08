c9ecddd62df91d411861f79cd8eb702d0561ff46
benyamin ardestani zadeh <benyaminardestanizadeh@outlook.com>
2024-01-08 17:48:38 +0330

    feat(header): get save each character in seter

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,30 +1,43 @@
 import { Button, Flex, Text } from "@radix-ui/themes";
 import { ReactSVG } from "react-svg";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Header = () => {
-  const [searchValue, setSearchValue] = useState<string | number | null>(null);
-  return (
-    <form>
-      <Flex direction={"column"} gap={"4"}>
-        <Flex gap={"2"} align={"center"} className="search-wrapper">
-          <MagnifyingGlassIcon
-            style={{ color: "whitesmoke", width: "25px", height: "25px" }}
-          />
-          <input className="search-input" placeholder="Search Notes" />
-        </Flex>
+  const [searchValue, setSearchValue] = useState<string>("");
 
-        <Flex gap={"5"} align={"center"} py={"3"}>
-          <Button size={"3"} color="yellow" style={{ cursor: "pointer" }}>
-            <Text>All</Text>
-          </Button>
-          <Button size={"3"} color="yellow" style={{ cursor: "pointer" }}>
-            <ReactSVG src="./svg/FolderIcon.svg" />
-          </Button>
-        </Flex>
+  // useEffect(() => {}, [searchValue]);
+
+  const handleSearchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+  return (
+    // <form>
+    <Flex direction={"column"} gap={"4"}>
+      <Flex gap={"2"} align={"center"} className="search-wrapper">
+        <MagnifyingGlassIcon
+          style={{ color: "whitesmoke", width: "25px", height: "25px" }}
+        />
+        <input
+          type="search"
+          className="search-input"
+          placeholder="Search Notes"
+          onChange={(event) => handleSearchInput(event)}
+          value={searchValue}
+        />
       </Flex>
-    </form>
+
+      <Flex gap={"5"} align={"center"} py={"3"}>
+        <Button size={"3"} color="yellow" style={{ cursor: "pointer" }}>
+          <Text>All</Text>
+        </Button>
+        <Button size={"3"} color="yellow" style={{ cursor: "pointer" }}>
+          <ReactSVG src="./svg/FolderIcon.svg" />
+        </Button>
+      </Flex>
+    </Flex>
+    // </form>
   );
 };
 
