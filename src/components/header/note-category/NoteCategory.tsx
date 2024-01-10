d08fcb90c7fc5827656a3f0a2b7dc6e1ca1f3ddd
benyamin ardestani zadeh <benyaminardestanizadeh@outlook.com>
2024-01-10 19:27:20 +0330

    feat: define note category for handle categories

---
+++ src/components/header/note-category/NoteCategory.tsx
@@ -0,0 +1,67 @@
+import { Box, Button, Dialog, Flex } from "@radix-ui/themes";
+import { ReactSVG } from "react-svg";
+import { TrashIcon } from "@radix-ui/react-icons";
+import { useAtom } from "jotai";
+import { categoryAtom } from "../../../store/note";
+import CategoryItemWrapper from "./category-item/CategoryItem";
+import NewCategory from "./new-category/NewCategory";
+import CustomDialog from "../../shared/dialog/CustomDialog";
+
+const NoteCategories = () => {
+  const [categoryItem] = useAtom(categoryAtom);
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <ReactSVG src="./svg/FolderIcon.svg" />
+      </Dialog.Trigger>
+      <Dialog.Content
+        style={{
+          backgroundColor: "#111111",
+          // boxShadow: " 1px 1px 4px 1px #ffea8a",
+        }}
+      >
+        <Flex justify={"between"} align={"center"}>
+          <Dialog.Title style={{ color: "whitesmoke" }}>
+            Categories
+          </Dialog.Title>
+          <Button
+            color="yellow"
+            style={{ width: "50px", height: "40px", cursor: "pointer" }}
+          >
+            <TrashIcon style={{ width: "25px", height: "25px" }} />
+          </Button>
+        </Flex>
+
+        <Flex direction={"column"} gap={"3"} mt={"5"}>
+          {categoryItem.map((item, index) => (
+            <CategoryItemWrapper
+              key={index}
+              title={item.title}
+              categoryItemNumbers={item.categoryItemNumbers}
+            />
+          ))}
+        </Flex>
+        <CustomDialog
+          triger={
+            <Box width={"100%"} mt={"4"}>
+              <Button
+                size={"4"}
+                color="yellow"
+                variant="outline"
+                style={{ width: "100%", cursor: "pointer" }}
+              >
+                Create New Category
+              </Button>
+            </Box>
+          }
+          content={(dismiss) => <NewCategory dismiss={dismiss} />}
+          backgroundColor="#0e0e0e"
+          boxShadow="1px 1px 4px 1px #f8f7f0"
+          maxWidth="60rem"
+        />
+      </Dialog.Content>
+    </Dialog.Root>
+  );
+};
+
+export default NoteCategories;
