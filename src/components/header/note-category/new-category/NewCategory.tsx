355aaca3451689e6228e836b07b1255f5e047910
benyamin ardestani zadeh <benyaminardestanizadeh@outlook.com>
2024-01-13 13:44:34 +0330

    feat(category): define dialog for create new category button

--- src/components/header/note-category/new-category/NewCategory.tsx
+++ src/components/header/note-category/new-category/NewCategory.tsx
@@ -1,12 +1,46 @@
 import { Button, Dialog, Flex, Text } from "@radix-ui/themes";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { CategoryItemProps } from "../../../../types/note.types";
+import { useAtom } from "jotai";
+import { categoryAtom } from "../../../../store/note";
 
 const NewCategory = ({ dismiss }: { dismiss: () => void }) => {
+  const [cardItems, setCardItem] = useAtom(categoryAtom);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CategoryItemProps>();
+
+  const submitNewCategory: SubmitHandler<CategoryItemProps> = (item) => {
+    if (item.title.trim() !== "") {
+      const newCategoryCard = {
+        title: item.title,
+        number: cardItems.length,
+      };
+      setCardItem([...cardItems, newCategoryCard]);
+    }
+    dismiss();
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(submitNewCategory)}>
       <Dialog.Title style={{ color: "whitesmoke" }}>New Category</Dialog.Title>
       <Flex direction={"column"} gap={"3"} mt={"4"}>
-        <input placeholder="Enter your category name" className="input-style" />
-        <Flex justify={"end"} gap={"2"}>
+        <input
+          {...register("title", {
+            required: "You have to type something",
+          })}
+          placeholder="Enter your category name"
+          className="input-style"
+          onChange={(e) => e.target.value}
+        />
+        {errors.title?.message && (
+          <Text as="label" color="tomato">
+            {errors.title.message}
+          </Text>
+        )}
+        <Flex justify={"end"} gap={"2"} mt={"9"}>
           <Dialog.Close>
             <Button
               type="button"
