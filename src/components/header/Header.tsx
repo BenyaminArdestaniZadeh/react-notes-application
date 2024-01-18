ffbd9b5f37c00aa7f3109b46c32af7009f03950b
benyamin ardestani zadeh <benyaminardestanizadeh@outlook.com>
2024-01-18 20:05:55 +0330

    refactor(header): implement section for show categories in header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,18 @@
 import { Button, Flex, Text } from "@radix-ui/themes";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
-import { useState } from "react";
-import { UseDebounce } from "../use-debounce/UseDebounce";
+
 import { useAtom } from "jotai";
-import { noteAtom } from "../../store/note";
+import { categoryItemAtom, noteAtom } from "../../store/note";
 import NoteCategories from "./note-category/NoteCategory";
 import DeleteButton from "../shared/delete-button/DeleteButton";
+import CustomDialog from "../shared/dialog/CustomDialog";
+import { ReactSVG } from "react-svg";
 
 const Header = () => {
-  const [searchValue, setSearchValue] = useState<string>("");
-  const debouncedValue = UseDebounce<string>(searchValue, 500);
+  // const [searchValue, setSearchValue] = useState<string>("");
+  // const debouncedValue = UseDebounce<string>(searchValue, 500);
   const [noteItem, setNoteItem] = useAtom(noteAtom);
+  const [categoryCardItems] = useAtom(categoryItemAtom);
 
   const handleSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const result = noteItem.filter((item) => {
@@ -37,15 +39,50 @@ const Header = () => {
       </Flex>
 
       <Flex width={"100%"} justify={"between"} align={"center"}>
-        <Flex gap={"5"} align={"center"} py={"3"}>
-          <Button size={"3"} color="yellow" style={{ cursor: "pointer" }}>
+        <Flex width={"100%"} gap={"5"} align={"center"}>
+          <Button size={"3"} color="yellow">
             <Text>All</Text>
           </Button>
-          <Button size={"3"} color="yellow" style={{ cursor: "pointer" }}>
-            <NoteCategories />
-          </Button>
+          {/* categories */}
+          <Flex
+            py={"2"}
+            px={"3"}
+            gap={"3"}
+            style={{
+              width: "88%",
+              borderRadius: "8px",
+            }}
+          >
+            {categoryCardItems &&
+              categoryCardItems.map((item) => (
+                <Button
+                  key={item.id}
+                  size={"3"}
+                  id={item.id.toString()}
+                  color="cyan"
+                  className="selected-category"
+                  // onClick={() => console.log(item.id)}
+                >
+                  <Text weight={"bold"}>{item.title}</Text>
+                </Button>
+              ))}
+          </Flex>
         </Flex>
-        <DeleteButton />
+        <Flex gap={"5"} align={"center"} py={"3"}>
+          <CustomDialog
+            triger={
+              <Button size={"3"} color="yellow">
+                <ReactSVG src="./svg/FolderIcon.svg" />
+              </Button>
+            }
+            content={(dismiss) => <NoteCategories dismiss={dismiss} />}
+            backgroundColor="#111111"
+          />
+          {/* <Button size={"3"} color="yellow" >
+            <NoteCategories />
+          </Button> */}
+          <DeleteButton />
+        </Flex>
       </Flex>
     </Flex>
   );
